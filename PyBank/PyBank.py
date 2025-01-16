fc62ae9cb16b11e6b61a56846ae8dd254aad8dc4
CymbalOfJoy <daniel.sterling.613@gmail.com>
2025-01-15 20:45:35 -0500

    Reorganized Repository to match expected structure

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,6 +1,3 @@
-# -*- coding: UTF-8 -*-
-"""PyBank Homework Starter File."""
-
 # Dependencies
 import csv
 import os
--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -1,6 +1,3 @@
-# -*- coding: UTF-8 -*-
-"""PyPoll Homework Starter File."""
-
 # Import necessary modules
 import csv
 import os
@@ -29,30 +26,19 @@ with open(file_to_load) as election_data:
 
     # Loop through each row of the dataset and process it
     for row in reader:
-
-        # Print a loading indicator (for large datasets)
-        print(". ", end="")
-
-        # Increment the total vote count for each row
         total_votes += 1
-
-
         # Get the candidate's name from the row
         candidate_name = row[2]
-
-
         # If the candidate is not already in the candidate list, add them
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
-
 
         # Add a vote to the candidate's count
         candidate_votes[candidate_name] += 1
 
 # Open a text file to save the output
 with open(file_to_output, "w") as txt_file:
-
     # Print the total vote count (to terminal)
     election_results = (
         f"\n\nElection Results\n"
@@ -63,7 +49,6 @@ with open(file_to_output, "w") as txt_file:
     
     # Write the total vote count to the text file
     txt_file.write(election_results)
-
 
     # Loop through the candidates to determine vote percentages and identify the winner
     for candidate in candidate_votes:
@@ -77,12 +62,10 @@ with open(file_to_output, "w") as txt_file:
             winning_count = votes
             winning_candidate = candidate
 
-
         # Print and save each candidate's vote count and percentage
-        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})/n"
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
         print(voter_output, end="")
         txt_file.write(voter_output)
-
 
     # Generate and print the winning candidate summary
     winning_candidate_summary= (
@@ -90,7 +73,6 @@ with open(file_to_output, "w") as txt_file:
          f"Winner: {winning_candidate}\n"
          f"-------------------------\n")
     print(winning_candidate_summary)
-
 
     # Save the winning candidate summary to the text file
     txt_file.write(winning_candidate_summary)
